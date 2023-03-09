a5e5229b68ce3f7956320b3cc2a0d478b68b5dec
Job Fernandez <jobandjeffy@gmail.com>
2023-03-09 10:53:11 +0530

    initial commit

---
+++ pages/about.vue
@@ -0,0 +1,3 @@
+<template>
+    <p class="text-3xl bg-blue-500">this is the about page</p>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,48 @@
+<template>
+ <div>
+  <video class="video" autoplay muted preload loop playsinline>
+            
+            <source
+                src="https://mazwai.com/videvo_files/video/free/2018-03/small_watermarked/180301_15_B_KowloonPark_06_preview.webm"
+                type="video/mp4"
+                poster="/background.png" />
+        
+    </video>
+    
+        <div id="overlay" style="background: rgba(0, 0, 0, 0.75);"></div>
+    
+
+
+<nav role="navigation" class="flex-container" id="splash">
+
+    
+    
+        <a href="projects" class="big-link">
+            &lt;projects /&gt;
+        </a>
+    
+        <a href="contact" class="big-link">
+            &lt;contact /&gt;
+        </a>
+    
+        <a href="portfolio" class="big-link">
+            &lt;portfolio /&gt;
+        </a>
+    
+        <a href="about" class="big-link">
+            &lt;who is job? /&gt;
+        </a>
+    
+
+    
+
+</nav>
+   </div>
+  
+</template>
+
+<script>
+export default {
+  name: 'IndexPage',
+}
+</script>
